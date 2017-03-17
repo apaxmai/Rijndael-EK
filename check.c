82e780f5e638e5d3874e5291fddb26c2ae928e10
Charles A. Morris <cmorris@cs.odu.edu>
2017-03-17 15:14:53 -0400

    tests

--- check.c
+++ check.c
@@ -1,8 +1,40 @@
 
-//NIST The Advanced Encryption Standard Algorithm Validation Suite (AESAVS)
-//Selected Entries
 
-//test_key
-//test_plaintext
-//test_ciphertext
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+
+#include "Rijndael-EK.h"
+
+//Testing Resources:
+// http://csrc.nist.gov/groups/STM/cavp/documents/aes/AESAVS.pdf
+
+int main(int argc, char **argv)
+{
+
+	unsigned int totalTests, testsPassed, testsFailed;
+	DIR *testsDirPtr;
+	struct dirent *testsDirEntry = NULL;
+
+	totalTests = 0;
+	testsPassed = 0;
+	testsFailed = 0;
+
+	testsDirPtr = opendir("./tests/");
+	if (! testsDirPtr) {
+		printf("Could not open tests directory. Failed!\n");
+		exit(-1);
+	}
+
+	while ((testsDirEntry = readdir(testsDirPtr))) {
+		if (testsDirEntry->d_name[0] != '.') {
+			printf("Running Test: %s\n", testsDirEntry->d_name);
+			// x/key  x/plaintext  x/ciphertext
+		}
+	}
+
+	closedir(testsDirPtr);
+
+	return 0;
+}
 
